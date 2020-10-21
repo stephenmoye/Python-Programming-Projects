47e55be31898d17b8a4630473092b26163de8e90
Stephen Moye <stephenmoye@gmail.com>
2020-10-20 19:23:04 -0700

    shipping charges completed

--- Chapter 3/shipping-charges.py
+++ Chapter 3/shipping-charges.py
@@ -7,3 +7,14 @@
 # Over 10 pounds $4.75
 # Write a program that asks the user to enter the weight of a package then displays the shipping
 # charges.
+
+weight = float(input('What is the weight of your object in lbs? '))
+
+if weight <= 2:
+    print(weight, "lbs costs $1.50")
+elif weight > 2 and weight <= 6:
+    print(weight, "lbs costs $3.00")
+elif weight > 6 and weight <= 10:
+    print(weight, "lbs costs $4.00")
+else:
+    print(weight, "lbs costs $4.75")
