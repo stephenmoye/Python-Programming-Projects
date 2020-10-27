c4d0b8e7f610b64cd2e384c751ed59dbe778b1d9
Stephen Moye <stephenmoye@gmail.com>
2020-10-27 10:50:59 -0700

    completed sum of numbers

--- Chapter 4/sum-of-numbers.py
+++ Chapter 4/sum-of-numbers.py
@@ -2,3 +2,14 @@
 # Write a program with a loop that asks the user to enter a series of positive numbers. The user
 # should enter a negative number to signal the end of the series. After all the positive numbers
 # have been entered, the program should display their sum.
+
+# ask for number
+num = int(input('Please enter a number: '))
+count = 0
+
+while num >= 0:
+    count += num
+    num = int(input(
+        'Please enter a + number to continue, or a - number to end program: '))
+
+print("total:", count)
