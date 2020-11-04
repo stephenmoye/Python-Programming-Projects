5cf4705c077e0e5ab90b6cc8ee5f06984d436a01
Stephen Moye <stephenmoye@gmail.com>
2020-11-03 18:52:50 -0800

    solved budget-analysis.py

--- Chapter 4/budget-analysis.py
+++ Chapter 4/budget-analysis.py
@@ -3,3 +3,14 @@
 # A loop should then prompt the user to enter each of his or her expenses for the month and keep
 # a running total. When the loop finishes, the program should display the amount that the user is
 # over or under budget.
+
+budget = float(input('What is your budget? '))
+total = 0
+
+while total < budget:
+  expense = float(input('What is the price of your purchase? '))
+  total += expense
+
+print('You are over budget!')
+print('Budget:', budget)
+print('Total:', total)
