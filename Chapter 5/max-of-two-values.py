32377ac5c231dafa7a68225546de1dc18b465e15
Stephen Moye <stephenmoye@gmail.com>
2020-12-01 21:31:49 -0800

    max of two values solved

--- Chapter 5/max-of-two-values.py
+++ Chapter 5/max-of-two-values.py
@@ -3,3 +3,13 @@
 # that is the greater of the two. For example, if 7 and 12 are passed as arguments to the function,
 # the function should return 12. Use the function in a program that prompts the user to enter two
 # integer values. The program should display the value that is the greater of the two.
+
+def max():
+    value1 = int(input("Enter your first value: "))
+    value2 = int(input("Enter your second value: "))
+    if value1 > value2:
+        print(value1, "is greater")
+    elif value2 > value1:
+        print(value2, "is greater")
+
+max()
